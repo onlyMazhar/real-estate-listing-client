54a27530114869e2061832d4588906d2bbbf0b61
Mazhar <mi.sabbir420@gmail.com>
2025-11-09 17:17:16 +0600

    making navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,45 @@ import './App.css'
 
 function App() {
 
+
+
   return (
     <>
-      
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button>
-          count is 
-        </button>
+      <div className="navbar bg-base-100 shadow-sm container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li> Home </li>
+              <li> All Properties </li>
+              <li> Add Properties </li>
+              <li> My Properties </li>
+              <li> My Ratings </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">HomeNest</a>
         </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 space-x-12">
+            <li> Home </li>
+            <li> All Properties </li>
+            <li> Add Properties </li>
+            <li> My Properties </li>
+            <li> My Ratings </li>
+          </ul>
+        </div>
+        <div className="dropdown dropdown-bottom dropdown-end navbar-end">
+          <div tabIndex={0} role="button" className="btn m-1">Click ⬇️</div>
+          <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
